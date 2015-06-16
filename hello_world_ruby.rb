3b0ea22405d8d5120c4e0a35b5efcf7f43815e5f
Will Sherman <vvj5@icloud.com>
2015-06-16 15:49:48 -0500

    Initial Commit, Ruby 'Hello World' done

---
+++ hello_world_ruby.rb
@@ -0,0 +1,12 @@
+print "Hello World"
+
+
+
+
+
+
+
+ # Write a function in Ruby and Javascript that prints out ‘Hello World’.
+# Write a working implementation of fizzbuzz in Ruby and in Javascript.
+# Write a working implementation of ‘age verifier’ in Ruby and in Javascript.
+# The verify age function will take a single argument ‘age’ and it will check if that age is less than 21. If it is less than 21 than the function will print ‘too young’ otherwise it’ll print ‘proceed’.
