2d0dbd32bea5c429371107936e048939c6937555
Will Sherman <vvj5@icloud.com>
2015-06-16 16:50:34 -0500

    Finished Ruby Age Verifier

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+// (1..100).each do |n|
+// if n % 3 && 5 == 0
+//   puts "FizzBuzz"
+// elsif n % 3 == 0
+//   puts "Fizz"
+// elsif n % 5 == 0
+//   puts "Buzz"
+// else puts n
+// end
+// end
