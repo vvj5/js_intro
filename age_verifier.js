5efb28254bd551df81270db4e6ca51d6041f687e
Will Sherman <vvj5@icloud.com>
2015-06-16 19:03:57 -0500

    Finished Ruby Age Verifier

---
+++ age_verifier.js
@@ -0,0 +1,16 @@
+var Age = function () {
+
+  ask user to input age
+
+  i = user input
+
+  if (i >= 21)
+    { console.log("Proceed") }
+  else { console.log("Too Young")}
+
+Age()
+
+// if (the condition)
+// {do this, computer!}
+// else if (some other condition)
+// {do this other thing};
--- age_verifier.rb
+++ age_verifier.rb
@@ -1,65 +1,8 @@
-class Start
-  def initialize
-    ask_age
-  end
-
-  def ask_age
-    puts "What is your age?"
-    @age = gets.chomp.to_i
-    ask_if_correct
-  end
-
-  def ask_if_correct
-    puts "You declared your age as #{@age}. Would you like to change this? (y/n)"
-    change = gets.chomp
-    if change.include? "y"
-      ask_age
-    else
-      evaluate_permissions
-    end
-  end
-
-  def evaluate_permissions
-    if @age >= 21
-      puts "#{l1_permissions} #{l2_permissions} #{l3_permissions}"
-      retry_message
-    elsif @age >= 18
-      puts "#{l1_permissions} #{l2_permissions}"
-      retry_message
-    elsif @age >=16
-      puts "#{l1_permissions}"
-      retry_message
-    else @age < 16
-      puts "#{l0_permissions}"
-      retry_message
-    end
-  end
-
-  def l3_permissions
-    puts "\nYou may go to nudie bars and you may drink booze."
-  end
-
-  def l2_permissions
-    puts "\nYou may vote and be sent to war to die."
-  end
-
-  def l1_permissions
-    puts "\nYou may drive a car and work like an urchin."
-  end
-
-  def l0_permissions
-    puts "\nYou are not yet ready, young Padawan"
-  end
-
-  def retry_message
-    puts "Press 'y' to try again or 'x' to exit"
-    change = gets.chomp
-    if change.include? "y"
-      ask_age
-    end
+def verify(age)
+  if age >= 21
+    puts "Proceed"
+  else puts "Too Young"
   end
 end
-Start.new
 
-
-
+verify(23)
