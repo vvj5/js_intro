bb2e638ca802b84249d70a516f37614b74c67a77
Will Sherman <vvj5@icloud.com>
2015-06-16 19:16:26 -0500

    JS Age Verifier Working

--- age_verifier.js
+++ age_verifier.js
@@ -1,16 +1,7 @@
-var Age = function () {
+var Age = function(i) {
+  if  (i >= 21)
+      { console.log("Proceed") }
+  else{ console.log("Too Young") }
+};
 
-  ask user to input age
-
-  i = user input
-
-  if (i >= 21)
-    { console.log("Proceed") }
-  else { console.log("Too Young")}
-
-Age()
-
-// if (the condition)
-// {do this, computer!}
-// else if (some other condition)
-// {do this other thing};
+Age(23)
